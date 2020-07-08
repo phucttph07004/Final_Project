5ee362d35d754cdb6d441aa05c73a9547af5618a
Truongnq249 <48799136+Truongnq249@users.noreply.github.com>
2020-07-08 22:29:28 +0700

    update homepage

---
+++ resources/views/frontend/layout/header.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- Plugin -->
+    <link rel="stylesheet" href="/plugins/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" href="/plugins/box/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="/plugins/carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="/plugins/carousel/owl.theme.default.min.css">
+
+    <!-- Icon -->
+    <link rel="stylesheet" href="/plugins/fontawesome/css/font-awesome.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Start Header-->
+
+        <header class="header">
+            <div class="header__top">
+                <div class="container">
+                    <div class="header__top-inner d-flex align-items-center justify-content-between">
+                        <div class="header__top-info d-flex align-items-center">
+                            <p class="header__top-welcome">Welcome to Something English Center</p>
+                            <a href="mailto:something@gmail.com" class="header__top-email"><i class="fa fa-envelope-o"></i>something@gmail.com</a>
+                            <a href="tel:0123456789" class="header__top-phoneNumber"><i class="fa fa-phone"></i>0123456789</a>
+                        </div>
+                        <ul class="header__top-account d-flex">
+                            <li class="account__item"><a href="login.html" class="account__item-link">Login</a></li>
+                            <li class="account__item"><a href="register.html" class="account__item-link">Register</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="header__main d-flex">
+                <div class="container">
+                    <div class="header__main-inner d-flex align-items-center justify-content-between h-100">
+                        <div class="header__logo">
+                            <a href="index.html">
+                                <img src="/images/Logo.png" alt="logo">
+                            </a>
+                        </div>
+                        <nav class="header__menu">
+                            <ul class="header__menu-list d-flex">
+                                <li class="header__menu-item"><a href="index.html" class="item-link active">Home</a></li>
+                                <li class="header__menu-item"><a href="about.html" class="item-link">About</a></li>
+                                <li class="header__menu-item"><a href="news.html" class="item-link">News</a></li>
+                                <li class="header__menu-item"><a href="" class="item-link">Test</a></li>
+                                <li class="header__menu-item"><a href="contact.html" class="item-link">Contact</a></li>
+                            </ul>
+                        </nav>
+                        <div class="hamburger">
+                            <i class="fa fa-bars"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- End Header -->
